99ef9a6a2c4dcfb32875c69b156a7e42450738c5
mgulham <mgulham@yahoo.com>
2019-07-20 13:38:02 -0700

    Wworking java applied. Score and wins being counted. Only red gem has on click. need to add on click for other 3 gems

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,8 @@ var redPoints = 0;
 var aquaPoints = 0;
 var greenPoints = 0;
 var purplePoints= 0;
+var wins = 0;
+var losses = 0;
 
 function gameReset(){ 
 redPoints = Math.ceil(Math.random() * 12); //random number from 1-12
@@ -13,16 +15,29 @@ greenPoints = Math.ceil(Math.random() * 12); //random number from 1-12
 purplePoints = Math.ceil(Math.random() * 12); //random number from 1-12
 randomNumber = (Math.ceil(Math.random() * 102)) + 18; //random number from 19-120
 $("#randomNumber" ).html(`${randomNumber}`);
+$("#score").html("Your Total Score Is: 0");
+userScore = 0;
 }
 
-$("#red").on("click", function (){
-let redPoints = Math.ceil(Math.random() * 12);
 
-if (redPoints > 0){
-    userscore = redPoints + userScore;
-    $("#score" ).html(`${userScore}`);
-    return
-    }
-    else {
-        return
-    }})
+function addPoints(){
+userScore = userScore + redPoints;
+$("#score").html(`Your Total Score Is: ${userScore}`)
+if (userScore == randomNumber){
+    alert("You won!");
+    gameReset();
+    wins++;
+    $("#wins").html(`Wins: ${wins}`)
+}
+else if (userScore > randomNumber){
+    alert("You Lost!");
+    gameReset();
+    losses++
+    $("#losses").html(`Losses: ${losses}`)
+}
+};
+
+$("#red").click(addPoints)
+
+
+gameReset();
